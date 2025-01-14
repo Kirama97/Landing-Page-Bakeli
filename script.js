a49d62c58b63224fa1b147ddd0cae651a3eba271
Kirama97 <officialthiam97@gmail.com>
2025-01-14 09:46:47 +0000

    burguer

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const burguer = document.querySelector('.burguer')
+const link = document.querySelector('.link')
+
+
+
+  
+
+burguer.onclick = function () {
+    link.classList.toggle('active');
+    header.style.backdropFilter = "blur(10px)"
+   
+ 
+}
+burguer.onclick = function () {
+  
+    link.classList.toggle('active');
+    
+}
